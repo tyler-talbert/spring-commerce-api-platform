36c680e38343475ecd49a73dfe82f333fac81650
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-09 21:05:38 -0800

    feat: add GET /users/{id} endpoint with DTO and 404 handling

--- src/main/java/com/codewithtyler/store/controllers/UserController.java
+++ src/main/java/com/codewithtyler/store/controllers/UserController.java
@@ -1,21 +1,37 @@
 package com.codewithtyler.store.controllers;
 
-import com.codewithtyler.store.entities.User;
+import com.codewithtyler.store.dtos.UserDto;
 import com.codewithtyler.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    @GetMapping("")
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(u -> new UserDto(u.getId(), u.getName(), u.getEmail()))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user =  userRepository.findById(id).orElse(null);
+        if (user == null)
+            return ResponseEntity.notFound().build();
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
+
     }
 
 }
--- src/main/java/com/codewithtyler/store/repositories/UserRepository.java
+++ src/main/java/com/codewithtyler/store/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.codewithtyler.store.repositories;
 
 import com.codewithtyler.store.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
