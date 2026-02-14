bd90245fbd64f5bf22d1070e052b4c24d43f7182
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-14 15:00:33 -0800

    feat: add POST /users endpoint for user creation

--- src/main/java/com/codewithtyler/store/controllers/UserController.java
+++ src/main/java/com/codewithtyler/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.codewithtyler.store.controllers;
 
+import com.codewithtyler.store.dtos.RegisterUserRequest;
 import com.codewithtyler.store.dtos.UserDto;
 import com.codewithtyler.store.mappers.UserMapper;
 import com.codewithtyler.store.repositories.UserRepository;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Set;
 
@@ -37,7 +39,22 @@ public class UserController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
 
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(
+            @RequestBody RegisterUserRequest request,
+            UriComponentsBuilder uriBuilder) {
+        var user = userMapper.toEntity(request);
+        userRepository.save(user);
+
+        var userDto = userMapper.toDto(user);
+        var uri = uriBuilder
+                .path("/users/{id}")
+                .buildAndExpand(userDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(userDto);
     }
 
 }
