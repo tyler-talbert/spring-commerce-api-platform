c7ea5bf7a118d1619564183b0ada479310b6577b
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-14 13:41:22 -0800

    feat: add ProductRepository queries for cateogry and filters

--- src/main/java/com/codewithtyler/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithtyler/store/repositories/ProductRepository.java
@@ -1,7 +1,19 @@
 package com.codewithtyler.store.repositories;
 
 import com.codewithtyler.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = "category")
+    @Query("SELECT p FROM Product p")
+    List<Product> findAllWithCategory();
+
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategoryId(Byte categoryId);
+
 }
