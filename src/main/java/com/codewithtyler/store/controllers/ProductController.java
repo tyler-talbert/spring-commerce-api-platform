4085c0c897dc3e43974d43d983a2b51101871120
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-14 18:33:32 -0800

    feat: add product create update delete endpoints

--- src/main/java/com/codewithtyler/store/controllers/ProductController.java
+++ src/main/java/com/codewithtyler/store/controllers/ProductController.java
@@ -3,10 +3,13 @@ package com.codewithtyler.store.controllers;
 import com.codewithtyler.store.dtos.ProductDto;
 import com.codewithtyler.store.entities.Product;
 import com.codewithtyler.store.mappers.ProductMapper;
+import com.codewithtyler.store.repositories.CategoryRepository;
 import com.codewithtyler.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.List;
 
 @RestController
@@ -15,6 +18,7 @@ import java.util.List;
 public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping("")
     public Iterable<ProductDto> getAllProducts(@RequestParam(name = "categoryId", required = false) Byte categoryId) {
@@ -38,5 +42,58 @@ public class ProductController {
 
         return ResponseEntity.ok(productMapper.toDto(product));
     }
+
+    @PostMapping("")
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto request,
+            UriComponentsBuilder uriBuilder
+            ) {
+        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
+        if (category == null)
+            return ResponseEntity.badRequest().build();
+
+        var product = productMapper.toEntity(request);
+        product.setCategory(category);
+        productRepository.save(product);
+
+        var productDto = productMapper.toDto(product);
+        var uri = uriBuilder
+                .path("/products/{id}")
+                .buildAndExpand(productDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductDto request
+    ) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null)
+            return ResponseEntity.notFound().build();
+
+        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
+        if (category == null)
+            return ResponseEntity.badRequest().build();
+
+        productMapper.update(request, product);
+        product.setCategory(category);
+        productRepository.save(product);
+
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null)
+            return ResponseEntity.notFound().build();
+
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/codewithtyler/store/mappers/ProductMapper.java
+++ src/main/java/com/codewithtyler/store/mappers/ProductMapper.java
@@ -4,9 +4,16 @@ import com.codewithtyler.store.dtos.ProductDto;
 import com.codewithtyler.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+
     @Mapping(target = "categoryId", source = "category.id")
     ProductDto toDto(Product product);
+
+    Product toEntity(ProductDto request);
+
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto request, @MappingTarget Product product);
 }
