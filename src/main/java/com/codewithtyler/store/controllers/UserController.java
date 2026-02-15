d6642f38985e7453086efa2e9a797924044259e5
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-14 22:34:40 -0800

    feat: add request validation to user creation endpoint

--- src/main/java/com/codewithtyler/store/controllers/UserController.java
+++ src/main/java/com/codewithtyler/store/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.codewithtyler.store.dtos.UpdateUserRequest;
 import com.codewithtyler.store.dtos.UserDto;
 import com.codewithtyler.store.mappers.UserMapper;
 import com.codewithtyler.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -46,7 +47,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(
-            @RequestBody RegisterUserRequest request,
+            @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder) {
         var user = userMapper.toEntity(request);
         userRepository.save(user);
