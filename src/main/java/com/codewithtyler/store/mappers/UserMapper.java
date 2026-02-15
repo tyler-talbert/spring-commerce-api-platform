f84c883c28dd6fe7eee4448ca61eee187ceea649
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-14 18:34:10 -0800

    chore: minor formatting cleanup

--- src/main/java/com/codewithtyler/store/mappers/UserMapper.java
+++ src/main/java/com/codewithtyler/store/mappers/UserMapper.java
@@ -10,6 +10,8 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+
     User toEntity(RegisterUserRequest request);
+
     void update(UpdateUserRequest request, @MappingTarget User user);
 }
