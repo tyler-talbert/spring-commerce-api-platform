acaad135a2f982c511aaa7a7d0df1db208c9ca4d
Tyler Talbert <tylertalbert02@gmail.com>
2026-02-15 17:34:53 -0800

    feat: add endpoints for creating carts and managing cart items

---
+++ src/main/java/com/codewithtyler/store/controllers/CartController.java
@@ -0,0 +1,79 @@
+package com.codewithtyler.store.controllers;
+
+import com.codewithtyler.store.dtos.AddItemToCartRequest;
+import com.codewithtyler.store.dtos.CartDto;
+import com.codewithtyler.store.dtos.CartItemDto;
+import com.codewithtyler.store.entities.Cart;
+import com.codewithtyler.store.entities.CartItem;
+import com.codewithtyler.store.mappers.CartMapper;
+import com.codewithtyler.store.repositories.CartRepository;
+import com.codewithtyler.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+    private final CartRepository cartRepository;
+    private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
+
+    @PostMapping("")
+    public ResponseEntity<CartDto> createCart(
+            UriComponentsBuilder uriBuilder
+    ) {
+        var cart = new Cart();
+        cartRepository.save(cart);
+
+        var cartDto = cartMapper.toDto(cart);
+        var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(cartDto);
+    }
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request
+    ) {
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var product = productRepository.findById(request.getProductId()).orElse(null);
+        if (product == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+
+        cartRepository.save(cart);
+
+        var cartItemDto = cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+    }
+
+
+}
